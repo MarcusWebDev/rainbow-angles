f82ce8b352d0751c79dfa5aa71cc9b28a00b8632
Marcus <brooksomarcus@gmail.com>
2018-11-06 22:29:28 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rainbowAngles/precache-manifest.4816f27b0daff92a7a807e3b2aa69a46.js"
+  "/precache-manifest.e23d0bccedb91b3f00a60ec55bd66391.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rainbowAngles/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
