2c665c078832c883032160ef9814dac91c2178cf
Marcus <brooksomarcus@gmail.com>
2018-11-29 03:03:04 -0500

    Text describing the angles will now only display once all of the angle images for that slide have loaded. While the images are loading, the text box will display 'Loading...'

--- src/components/Carousel/ImageBox.js
+++ src/components/Carousel/ImageBox.js
@@ -5,12 +5,12 @@ import './ImageBox.css';
 const ImageBox = ({pictures, index, onClick, onLoad, onError}) => {
 	const imageArray = pictures[index].map((picture, i) => {
 		return (
-			<img key={i} src={pictures[index][i]} onClick={() => onClick(pictures[index][i])} onLoad={onLoad} onError={onError}/>
+			<img key={i} src={pictures[index][i]} onClick={() => onClick(pictures[index][i])} />
 		);
 	})
 
 	return (
-		<div className="imageContainer" >
+		<div className="imageContainer" onLoad={onLoad} onError={onError}>
 			{imageArray}
 		</div>
 	);
