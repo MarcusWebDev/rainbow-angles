230dafc2208133926a679f1cab4f4a6e4256622d
Marcus <brooksomarcus@gmail.com>
2018-12-11 14:19:01 -0500

    Creating loading boxes when the dots are used to navigate through the carousel

--- src/components/Carousel/Dots.js
+++ src/components/Carousel/Dots.js
@@ -6,7 +6,19 @@ const Dots = ({pictures, index, navigateToTargetSlide}) => {
 	const dotsArray = pictures.map((picture, i) => {
 		index === i ? className += ' dot-active' : className = 'dot';
 		return (
-			<div className={className} key={i} onClick={() => navigateToTargetSlide(i)}>
+			<div 
+				className={className} 
+				key={i} 
+				onClick={
+					() => {
+						navigateToTargetSlide(i);
+						for (let j = 0; j < document.getElementsByClassName('imageID').length; j++) {
+							document.getElementsByClassName('imageID')[j].setAttribute("style", "display: none;"); 
+							document.getElementsByClassName('boxID')[j].setAttribute("style", "display: flex;");
+						}
+					}
+				}
+			>
 			</div>
 		);
 	})
